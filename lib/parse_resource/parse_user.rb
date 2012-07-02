454c1e7c64181a338fb657992b55131f88b35264
Chris McCann <testflyjets@gmail.com>
2012-07-01 21:38:22 -0700

    Fixes bug in using settings method

--- lib/parse_resource/parse_user.rb
+++ lib/parse_resource/parse_user.rb
@@ -8,10 +8,9 @@ class ParseUser < ParseResource::Base
   # validates_with ParseUserValidator, :on => :create, :on => :save
 
   def self.authenticate(username, password)
-    self.settings
     base_uri   = "https://api.parse.com/1/users"
-    app_id     = settings['app_id']
-    master_key = settings['master_key']
+    app_id     = self.settings['app_id']
+    master_key = self.settings['master_key']
     resource = RestClient::Resource.new(base_uri, app_id, master_key)
     
     begin
